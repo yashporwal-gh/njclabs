afd36bac2ac67d60a38a7796930dcc7f2e91a194
yashporwal-gh <52754742+yashporwal-gh@users.noreply.github.com>
2021-08-05 11:48:46 +0530

    Add files via upload

---
+++ yashporwal_njclabs.py
@@ -0,0 +1,28 @@
+import sqlite3
+connection=sqlite3.connect('Database_Movies.db')
+cursor=connection.cursor()
+
+#create query
+query_command=""" CREATE TABLE IF NOT EXISTS Movies(movie_name TEXT PRIMARY KEY,lead_actor TEXT,actress TEXT,year_of_release INTEGER,director_name TEXT) """
+
+cursor.execute(query_command)
+
+#insert query
+cursor.execute(" INSERT INTO Movies VALUES ('Pulp Fiction','Tim Roth','Amanda Plummer',1994,'Quentin Tarantino')")
+cursor.execute(" INSERT INTO Movies VALUES ('The Pianist','Adrien Brody','Emilia Fox',2002,'Roman Polanski')")
+cursor.execute(" INSERT INTO Movies VALUES ('Inception','Leonardo DiCaprio','Elliot Page',2010,'Christopher Nolan')")
+cursor.execute(" INSERT INTO Movies VALUES ('Joker','Joaquin Phoenix','Zazie Beetz',2019,'Todd Phillips')")
+cursor.execute(" INSERT INTO Movies VALUES ('Fight Club','Brad Pitt','Helena Bonham Carter',1999,'David Fincher')")
+
+
+#select query
+cursor.execute("SELECT * FROM Movies")
+result1=cursor.fetchall()
+
+cursor.execute("SELECT * FROM Movies WHERE lead_actor='Leonardo DiCaprio' ")
+result2=cursor.fetchall()
+
+#print the results
+print(result1)
+
+print(result2)
